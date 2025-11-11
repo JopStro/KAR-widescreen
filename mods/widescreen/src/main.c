e6b3544365b899a3df0e65bd6547c7c2c7aad4b0
JoStro <joseph.strong@live.co.uk>
2025-11-11 21:05:33 +0000

    Rejig Main File

--- mods/widescreen/src/main.c
+++ mods/widescreen/src/main.c
@@ -2,18 +2,16 @@
 
 #define MOD_NAME "Widescreen Support"
 
-int is_widescreen = 0; // variable that is updated by the player in the in-game settings menu via the ModMenu defined below.
+int is_widescreen = 0;
 
 char ModName[] = MOD_NAME;      // Name of the mod.
 char ModAuthor[] = "JoStro"; // Creator of the mod.
 char ModVersion[] = "v0.1";     // Version of the mod.
 
 
-// Creates a menu that appears in the in-game Settings menu.
-// Menus may be nested by setting the OptionDesc::kind to OPTKIND_MENU
 OptionDesc ModSettings = {
     .name = "Aspect Ratio",
-    .description = "Change The Game's Aspect Ratio For Modern Displays!",
+    .description = "Change the game's aspect ratio for modern displays!",
     .kind = OPTKIND_VALUE,
     .val = &is_widescreen,
     .value_num = 2,
@@ -23,66 +21,8 @@ OptionDesc ModSettings = {
     }
 };
 
-// Runs immediately after the mod file is loaded.
-// Calls to HSD_MemAlloc in THIS function specifically wil persist throughout the entire runtime of the game.
-// All calls to HSD_MemAlloc from elsewhere will return an allocation that exists only within the current scene.
-void OnBoot()
-{
-}
-
-// Runs on boot when hoshi creates save data for the mod.
-// Initialize default save file values here.
-void OnSaveInit()
-{
-}
-
 // Runs on startup after any save data is loaded into memory.
 // This callback is executed regardless of if a memory card is inserted or contained existing save data.
 void OnSaveLoaded()
-{
-}
-
-// Runs when entering the main menu.
-void OnMainMenuLoad()
-{
-}
-
-// Runs when entering the player select menu.
-// Currently only executes when entering city trial player select.
-void OnPlayerSelectLoad()
-{
-}
-
-// Runs upon entering a 3D game. Can be either Air Ride or City Trial. Must be explicity checked using Gm_IsInCity().
-// Players, riders, their machines, and the map have all been instantiated by the time this is executed.
-void On3DLoad()
-{
-}
-
-// Runs when pausing the match. The index of the pausing player is passed in as an argument.
-void On3DPause(int pause_ply)
-{
-}
-
-// Runs when unpausing the match.
-void On3DUnpause()
-{
-}
-
-// Runs when exiting a match.
-void On3DExit()
-{
-}
-
-// Runs every scene change.
-// The memory heap is destroyed and recreated every scene change, meaning HSD objects
-// such as CObj's (camera) and JObj's (models) will not persist across them.
-// This hook can be used to recreate processes/objects that should always be running.
-void OnSceneChange()
-{
-}
-
-// Runs every game tick, even when the game is paused normally or via debug mode.
-void OnFrame()
 {
 }

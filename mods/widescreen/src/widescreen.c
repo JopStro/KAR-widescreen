05287e1f776a2750ae441c13b48526a8434e5189
JoStro <joseph.strong@live.co.uk>
2025-11-11 21:12:56 +0000

    Add Header and Setting cb

--- mods/widescreen/src/widescreen.c
+++ mods/widescreen/src/widescreen.c
@@ -1,18 +1,16 @@
 #include "hoshi/settings.h"
 
-#define MOD_NAME "Widescreen Support"
-
-int is_widescreen = 0;
+#include "widescreen.h"
 
 char ModName[] = MOD_NAME;      // Name of the mod.
 char ModAuthor[] = "JoStro"; // Creator of the mod.
 char ModVersion[] = "v0.1";     // Version of the mod.
 
-
 OptionDesc ModSettings = {
     .name = "Aspect Ratio",
     .description = "Change the game's aspect ratio for modern displays!",
     .kind = OPTKIND_VALUE,
+    .on_change = &Wide_ChangeSetting,
     .val = &is_widescreen,
     .value_num = 2,
     .value_names = (char *[]){
@@ -25,4 +23,8 @@ OptionDesc ModSettings = {
 // This callback is executed regardless of if a memory card is inserted or contained existing save data.
 void OnSaveLoaded()
 {
+}
+
+void Wide_ChangeSetting(int val) {
+    return;
 }
---
+++ mods/widescreen/src/widescreen.h
@@ -0,0 +1,10 @@
+#ifndef WIDESCREEN_H
+#define WIDESCREEN_H
+
+#define MOD_NAME "Widescreen Support"
+
+int is_widescreen = 0;
+
+void Wide_ChangeSetting(int val);
+
+#endif // WIDESCREEN_H
